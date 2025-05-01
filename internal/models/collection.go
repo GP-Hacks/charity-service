2dfa91e4a7e15fbcc80eed3a38ed5be4e8f0dbef
sergeyiksanov <sergeyiksanov25@gmail.com>
2025-05-01 20:35:38 +0300

    create domain models

--- internal/models/collection.go
+++ internal/models/collection.go
@@ -1,7 +1,20 @@
 package models
 
 type Collection struct {
-	ID           int
+	ID           int64
+	CategoryID   int64
+	Name         string
+	Description  string
+	Organization string
+	Phone        string
+	Website      string
+	Goal         int
+	Current      int
+	Photo        string
+}
+
+type CollectionWithCategory struct {
+	ID           int64
 	Category     string
 	Name         string
 	Description  string
@@ -11,4 +24,19 @@ type Collection struct {
 	Goal         int
 	Current      int
 	Photo        string
+}
+
+func (c *Collection) ToCollectionWithCategory(category string) *CollectionWithCategory {
+	return &CollectionWithCategory{
+		ID:           c.ID,
+		Category:     category,
+		Name:         c.Name,
+		Description:  c.Description,
+		Organization: c.Organization,
+		Phone:        c.Phone,
+		Website:      c.Website,
+		Goal:         c.Goal,
+		Current:      c.Current,
+		Photo:        c.Photo,
+	}
 }
